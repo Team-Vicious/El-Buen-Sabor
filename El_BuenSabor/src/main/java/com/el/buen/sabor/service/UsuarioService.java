2323f89c4bc07bcc8e863f5cf5efbaa7d6f399b9
luciano <lucianogrilli03@gmail.com>
2021-04-24 23:35:26 -0300

    fix un error del proyecto

--- El_BuenSabor/src/main/java/com/el/buen/sabor/service/UsuarioService.java
+++
@@ -1,8 +0,0 @@
-package com.el.buen.sabor.service;
-
-import com.el.buen.sabor.entity.Usuario;
-import com.formaciondbi.microservicios.generics.services.Services;
-
-public interface UsuarioService extends Services<Usuario, Long>{
-	public Usuario buscarUsuario();
-}
