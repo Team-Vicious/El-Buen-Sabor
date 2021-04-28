1a202575d7d9593564bd74f1e46293b0ac96335d
luciano <lucianogrilli03@gmail.com>
2021-04-28 12:09:48 -0300

    get pedido-clienteid agregado

--- El_buen_sabor/src/main/java/com/el/buen/sabor/service/PedidoServiceImpl.java
+++ El_buen_sabor/src/main/java/com/el/buen/sabor/service/PedidoServiceImpl.java
@@ -1,15 +1,23 @@
 package com.el.buen.sabor.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.el.buen.sabor.entity.Pedido;
-import com.el.buen.sabor.repository.ArticuloInsumoRepository;
+import com.el.buen.sabor.repository.PedidoRepository;
 import com.formaciondbi.microservicios.generics.services.ServicesImpl;
 
 @Service
 public class PedidoServiceImpl extends ServicesImpl<Pedido, Long> implements PedidoService{
 
 	@Autowired
-	ArticuloInsumoRepository repository;
+	PedidoRepository repository;
+
+	@Override
+	public List<Pedido> findPedidosByClienteId(Long id) {
+		
+		return repository.findPedidosByClienteId(id);
+	}
 }
